b53f2f4583a6be7b5b9fd8a42fc51954a72488ad
DalpatRathore <69510006+DalpatRathore@users.noreply.github.com>
2025-04-23 20:48:32 +0530

    feat: customize ui components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,23 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Progress } from "@/components/ui/progress";
+import { Textarea } from "@/components/ui/textarea";
+
 export default function Home() {
   return (
-    <div className="">
-      <h1>Dalpat Rathore</h1>
+    <div className="flex flex-col gap-4 p-10">
+      <div className="">
+        <Button variant={"elevated"}>I am button</Button>
+      </div>
+      <div className="">
+        <Input></Input>
+      </div>
+      <div className="">
+        <Textarea></Textarea>
+      </div>
+      <div className="">
+        <Progress value={50} />
+      </div>
     </div>
   );
 }
