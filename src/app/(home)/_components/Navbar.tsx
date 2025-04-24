ed63983beb3749d1594e9b4ee07304b0312cbca8
DalpatRathore <69510006+DalpatRathore@users.noreply.github.com>
2025-04-24 10:16:44 +0530

    feat: side bar for mobile

--- src/app/(home)/_components/Navbar.tsx
+++ src/app/(home)/_components/Navbar.tsx
@@ -1,10 +1,11 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import { usePathname } from "next/navigation";
 import { Poppins } from "next/font/google";
 import Link from "next/link";
 import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
+import NavbarSidebar from "./NavbarSidebar";
 
 const poppins = Poppins({
   subsets: ["latin"],
@@ -60,6 +61,8 @@ const NavbarItem = ({ href, children, isActive }: NavbarItemProps) => {
 
 const Navbar = () => {
   const pathname = usePathname();
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
   return (
     <nav className="h-20 flex border-b justify-between font-medium bg-white">
       <Link href={"/"} className="pl-6 flex items-center">
@@ -67,7 +70,12 @@ const Navbar = () => {
           funroad
         </span>
       </Link>
-      <div className="hiddend md:flex items-center gap-4 pr-6">
+      <NavbarSidebar
+        items={NAVBAR_ITEMS}
+        open={isSidebarOpen}
+        onOpenChange={setIsSidebarOpen}
+      />
+      <div className="hidden lg:flex items-center gap-4 pr-6">
         {NAVBAR_ITEMS.map(item => (
           <NavbarItem
             key={item.href}
@@ -77,6 +85,44 @@ const Navbar = () => {
             {item.children}
           </NavbarItem>
         ))}
+      </div>
+      <div className="hidden lg:flex items-center">
+        <Button
+          asChild
+          variant={"secondary"}
+          className="border-l border-t-0 border-r-0 px-12 h-full rounded-none bg-white hover:bg-pink-400 transition-colors text-lg"
+        >
+          <Link href={"/sign-in"}>Login</Link>
+        </Button>
+        <Button
+          asChild
+          variant={"secondary"}
+          className="border-l border-t-0 border-r-0 px-12 h-full rounded-none bg-black text-white hover:bg-pink-400 hover:text-black transition-colors text-lg"
+        >
+          <Link href={"/sign-up"}>Start Selling</Link>
+        </Button>
+      </div>
+      <div className="lg:hidden flex items-center pr-6">
+        <Button
+          variant={"outline"}
+          className="bg-transparent hover:bg-transparent rounded-full hover:border-primary border-transparent px-3.5 text-lg"
+          onClick={() => setIsSidebarOpen(true)}
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-6 w-6"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+              d="M4 6h16M4 12h16m-7 6h7"
+            />
+          </svg>
+        </Button>
       </div>
     </nav>
   );
---
+++ src/app/(home)/_components/NavbarSidebar.tsx
@@ -0,0 +1,63 @@
+import { ScrollArea } from "@/components/ui/scroll-area";
+import {
+  Sheet,
+  SheetContent,
+  SheetHeader,
+  SheetTitle,
+} from "@/components/ui/sheet";
+import Link from "next/link";
+import React from "react";
+
+type NavbarSidebarProps = {
+  items: {
+    href: string;
+    children: React.ReactNode;
+  }[];
+  open: boolean;
+  onOpenChange: (open: boolean) => void;
+};
+
+const NavbarSidebar = ({ items, open, onOpenChange }: NavbarSidebarProps) => {
+  return (
+    <Sheet open={open} onOpenChange={onOpenChange}>
+      <SheetContent side="left" className="p-0 transition-none">
+        <SheetHeader className="p-4 border-b">
+          <div className="flex items-center justify-between">
+            <SheetTitle className="flex items-center gap-2">Menu</SheetTitle>
+          </div>
+        </SheetHeader>
+        <ScrollArea className="flex flex-col overflow-y-auto h-full pb-2">
+          {items.map(item => (
+            <Link
+              key={item.href}
+              href={item.href}
+              className="w-full text-left p-4 text-lg font-medium hover:bg-black hover:text-white transition-colors flex items-center"
+              onClick={() => onOpenChange(false)}
+            >
+              {item.children}
+            </Link>
+          ))}
+          <div className="border-t">
+            <Link
+              className="w-full text-left p-4 text-lg font-medium hover:bg-black hover:text-white transition-colors flex items-center"
+              href={"/sign-in"}
+              onClick={() => onOpenChange(false)}
+            >
+              Login
+            </Link>
+
+            <Link
+              className="w-full text-left p-4 text-lg font-medium hover:bg-black hover:text-white transition-colors flex items-center"
+              href={"/sign-up"}
+              onClick={() => onOpenChange(false)}
+            >
+              Start Selling
+            </Link>
+          </div>
+        </ScrollArea>
+      </SheetContent>
+    </Sheet>
+  );
+};
+
+export default NavbarSidebar;
